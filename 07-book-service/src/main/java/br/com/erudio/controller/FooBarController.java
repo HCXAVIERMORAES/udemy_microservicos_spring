d1feb1dfd5d053a0f819ef7870a51594cf80ec6e
HCXAVIERMORAES <hcxaviermoraes@outlook.com>
2022-08-08 18:36:48 -0300

    commite seção 11
    circuit break com resilience4j

---
+++ 07-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -0,0 +1,39 @@
+package br.com.erudio.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
+//para simular a erros e commo usar o resilece4j
+@RestController 
+@RequestMapping("book-service")
+public class FooBarController {
+	
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+	
+
+	@GetMapping("/foo-bar")
+	//@Retry(name = "foo-bar",fallbackMethod = "fallbackMethod")
+	//@CircuitBreaker(name = "foo-bar",fallbackMethod = "fallbackMethod")
+	//@RateLimiter(name = "default")
+	@Bulkhead(name = "default")
+	public String fooBar() {
+		//simulando um erro com uma url que não exist
+		//new RestTemplate().getForEntity("http://localhost:8080/foobar", String.class);
+		//return "Foo-Bar!!!";
+		//pegando o erro e mostrando-o
+		logger.info("recebida solicitação para foo-bar");
+		//var response =new RestTemplate().getForEntity("http://localhost:8080/foobar", String.class);
+				//return response.getBody();
+		return "Foo-Bar!!!";
+	}
+	
+	public String fallbackMethod(Exception ex) {
+		return "fallbackMethod foo-bar!!!";
+	}
+}
